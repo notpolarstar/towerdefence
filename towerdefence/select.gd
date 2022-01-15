ce96762e32c4940061722ecbf81cba18b8a99d96
Tiziano <55955510+notpolarstar@users.noreply.github.com>
2022-01-15 18:40:53 +0100

    Semaine 2

--- towerdefence/select.gd
+++ towerdefence/select.gd
@@ -1,13 +1,14 @@
 extends Control
 onready var tween = get_node("Tween")
 onready var colorrect = get_node("KinematicBody2D/ColorRect")
+onready var quitter = get_node("ConfirmationDialog")
 var x = 0
 var y = 0
 
 func _ready():
 	colorrect.visible = 0
 
-func _process(delta):
+func _process(_delta):
 	var mos_pos = get_global_mouse_position()
 	x = int(mos_pos[0]/96)*96
 	y = int(mos_pos[1]/96)*96
@@ -20,3 +21,9 @@ func _process(delta):
 		tween.interpolate_property(colorrect,"modulate",Color(1,1,1,0.75),Color(1,1,1,0.4),1,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		tween.start()
 		colorrect.visible = 1
+	if (Input.is_action_just_pressed("fullscreen")):
+		OS.window_fullscreen = false
+		quitter.popup()
+
+func _on_ConfirmationDialog_confirmed():
+	get_tree().change_scene("res://EcranTitre.tscn");
